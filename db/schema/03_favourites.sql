eb8a1872d50ffd1ef9c7c0745b504e2dd7b27265
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-09 23:01:42 +0000

    asd

--- db/schema/03_favourites.sql
+++ db/schema/03_favourites.sql
@@ -4,4 +4,4 @@ DROP TABLE IF EXISTS favourites CASCADE;
 CREATE TABLE favourites (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE,
-)
+);
