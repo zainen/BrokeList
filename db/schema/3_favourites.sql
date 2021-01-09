18f8d7f252e1bf4bf1d28d5808b030ded454ef2a
Aqil Ebrahim <aqilebrahim@gmail.com>
2021-01-09 22:58:22 +0000

    fixed commits

--- db/schema/3_favourites.sql
+++
@@ -1,8 +0,0 @@
--- Drop and recreate Favourites table
-
-DROP TABLE IF EXISTS favourites CASCADE;
-
-CREATE TABLE favourites (
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE,
-);
