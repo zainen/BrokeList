38638076277d8bdff3e94b6c16a4d1bac9b5dadf
zainen <zainensuzuki@gmail.com>
2021-01-09 19:43:49 +0000

    get/post requests

--- server.js
+++ server.js
@@ -46,10 +46,42 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SERVER GET REQUESTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/new-post", (req, res) => {
+  res.render("index");
+});
+app.get("/view-my-listings", (req, res) => {
+  res.render("index");
+});
+app.get("/alter-listing", (req, res) => {
+  res.render("index");
+});
+// OR JUST /:product_id
+app.get("/product/:product_id", (req, res) => {
+  res.render("index");
+});
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SERVER POST REQUESTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+app.post('/', (req, res) => {
+
+});
+app.post('/', (req, res) => {
+
+});
+app.post('/', (req, res) => {
+
+});
+app.post('/', (req, res) => {
+
+});
+app.post('/', (req, res) => {
+
+});
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SERVER LISTENING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
