be835b88a0bc37c66b94f805f03ec5fda47adb19
Aqil Ebrahim <aqilebrahim@gmail.com>
2021-01-11 22:00:23 +0000

    updated seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,7 +3,8 @@
 INSERT INTO users (name, email, phone, is_admin)
 VALUES ('Johnny', 'johnny@johnny.com', '7807807808', TRUE),
   ('Zainen', 'zainen@zainen.com', '4034034033', TRUE),
-  ('Aqil', 'aqil@aqil.com', '5875875877', FALSE),
-  ('Martha', 'martha@marha.com', '4164164166', TRUE),
-  ('Stewart', 'stewart@stewart.com', '8048048044', FALSE)
+  ('Aqil', 'aqil@aqil.com', '5875875877', TRUE),
+  ('Martha', 'martha@marha.com', '4164164166', FALSE),
+  ('Stewart', 'stewart@stewart.com', '8048048044', FALSE),
+  ('Jim', 'jim@stewart.com', '8055048044', FALSE)
   ;
