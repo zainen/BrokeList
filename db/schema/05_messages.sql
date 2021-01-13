8a5cd895addd62f33c0e669627ae32a4f9bd0f48
zainen <zainensuzuki@gmail.com>
2021-01-13 03:31:36 +0000

    fix cascase

--- db/schema/05_messages.sql
+++ db/schema/05_messages.sql
@@ -6,6 +6,6 @@ CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   buyer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCASE,
+  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE,
   message TEXT
 )
