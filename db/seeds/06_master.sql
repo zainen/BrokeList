91463faed5db9630a780952ef2e51668e67b8c64
zainen <zainensuzuki@gmail.com>
2021-01-14 01:05:16 +0000

    fix message send by user to another not connected to listing

--- db/seeds/06_master.sql
+++ db/seeds/06_master.sql
@@ -59,5 +59,5 @@ VALUES (1,2,4, 'Hey can I buy this?');
 INSERT into messages (buyer_id, seller_id, listing_id, message)
 VALUES (1,2,4, 'Hey can I buy this?');
 INSERT into messages (buyer_id, seller_id, listing_id, message)
-VALUES (2,1,4, 'Hey can I buy this?');
+VALUES (2,1,1, 'Hey can I buy this?');
 
