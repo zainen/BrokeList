4de134d6a44d2432b5916b647d53ca11d6f19a64
zainen <zainensuzuki@gmail.com>
2021-01-15 04:44:08 +0000

    lint helpers

--- helperFunctionsSQL.js
+++ helperFunctionsSQL.js
@@ -1,150 +1,146 @@
-const { response } = require("express")
+const { response } = require("express");
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
-// const getUserId = (user, db) => {
 
-// }
 const addFavourite = (user, listing) => {
-  const arr = [user, listing]
+  const arr = [user, listing];
   return db.query(`
   INSERT INTO favourites (user_id, listing_id)
-  VALUES ($1, $2)
-  `, arr)
+  VALUES ($1, $2);
+  `, arr);
 };
 exports.addFavourite = addFavourite;
 
+
 const checkForFavourite = (user, listing) => {
-  const arr = [user, listing]
+  const arr = [user, listing];
   return db.query(`
   SELECT *
   FROM favourites
-  WHERE user_id = $1 AND listing_id = $2
-  `, arr)
-}
-exports.checkForFavourite= checkForFavourite;
+  WHERE user_id = $1 AND listing_id = $2;
+  `, arr);
+};
+exports.checkForFavourite = checkForFavourite;
 
 const checkUser = (id) => {
-  const value = [id]
+  const value = [id];
   return db.query(`
   SELECT * FROM users
   JOIN listings ON users.id = user_id
-  WHERE user_id = $1;`, value)
+  WHERE user_id = $1;`, value);
 };
 exports.checkUser = checkUser;
 
 const deleteListing = (id) => {
-  const value = [id]
+  const value = [id];
   return db.query(`
   DELETE FROM listings
-  WHERE id = $1
-  `, value)
+  WHERE id = $1;
+  `, value);
 };
 exports.deleteListing = deleteListing;
 
-// filterFavourite
 const favourites = (user) => {
-  const values = [user]
+  const values = [user];
   return db.query(`
   SELECT *
   FROM favourites
   JOIN listings ON listings.id = listing_id
   JOIN photos ON listings.id = photos.listing_id
   WHERE favourites.user_id = $1 AND is_sold = false;
-  `, values)
+  `, values);
 };
 exports.favourites = favourites;
 
 const filterPrice = (arr, min, max) => {
-  const newArr = []
+  const newArr = [];
   arr.forEach((item) => {
     if (item.price_in_cents >= min && item.price_in_cents <= max) {
-      newArr.push(item)
+      newArr.push(item);
     }
-  })
-  return newArr
+  });
+  return newArr;
 };
 exports.filterPrice = filterPrice;
 
 const getBuyerInfo = (id) => {
-  const arr = [id]
+  const arr = [id];
   return db.query(`
   SELECT *
   FROM users
   WHERE id = $1
-  `, arr)
+  `, arr);
 };
 exports.getBuyerInfo = getBuyerInfo;
 
 const getMessages = (seller_id) => {
-  const value = [seller_id]
+  const value = [seller_id];
   return db.query(`
   SELECT * FROM messages
   JOIN users ON users.id = seller_id
   JOIN listings ON listings.id = listing_id
   WHERE seller_id = $1 OR buyer_id = $1;
-  `, value)
-}
+  `, value);
+};
 exports.getMessages = getMessages;
 
 const getParticularMessage = (seller_id, user_id, listing_id) => {
-  const arr = [seller_id, user_id, Number(listing_id)]
-  console.log(arr)
+  const arr = [seller_id, user_id, Number(listing_id)];
   return db.query(`
   SELECT * FROM messages
   JOIN users ON users.id = seller_id
   JOIN listings ON listings.id = listing_id
   WHERE seller_id = $1 AND buyer_id = $2 AND listing_id = $3;
-  `, arr)
+  `, arr);
 };
 
 exports.getParticularMessage = getParticularMessage;
 
 const getListingByListing_id = (id) => {
-  const arr = [id]
+  const arr = [id];
   return db.query(`
   SELECT *
   FROM listings
   JOIN users ON users.id = user_id
   WHERE listings.id = $1;
-  `, arr)
-}
-exports.getListingByListing_id = getListingByListing_id
+  `, arr);
+};
+exports.getListingByListing_id = getListingByListing_id;
 
 
-// listings
 const listings = () => {
   return db.query(`
   SELECT *
   FROM listings
   JOIN photos ON listings.id = listing_id
   WHERE is_sold = false
-  ORDER BY listing_id DESC
-  `)
+  ORDER BY listing_id DESC;
+  `);
 };
 exports.listings = listings;
 
-const messageSeller = (buyer_id, seller_id, listing_id, message) => {
-  const newArr = [buyer_id, seller_id, listing_id, message]
 
+const messageSeller = (buyer_id, seller_id, listing_id, message) => {
+  const newArr = [buyer_id, seller_id, listing_id, message];
   return db.query(`
   INSERT INTO messages (buyer_id, seller_id, listing_id, message)
-  VALUES ($1, $2, $3, $4)
-  `, newArr)
+  VALUES ($1, $2, $3, $4);
+  `, newArr);
 };
 exports.messageSeller = messageSeller;
+
 
 const myListings = () => {
   return db.query(`
   SELECT * FROM listings
   JOIN photos ON listing_id = listings.id
   ORDER BY listing_id DESC;
-  `)
+  `);
 };
 exports.myListings = myListings;
-
 
 
 const newListing = (arr) => {
@@ -152,10 +148,9 @@ const newListing = (arr) => {
   INSERT INTO listings (user_id, title, price_in_cents, description)
   VALUES ($1, $2, $3, $4)
   RETURNING id;
-  `, arr)
+  `, arr);
 };
 exports.newListing = newListing;
-
 
 
 const newListingPhoto = (items) => {
@@ -163,28 +158,29 @@ const newListingPhoto = (items) => {
   INSERT INTO photos (location, listing_id)
   VALUES ($1, $2)
   RETURNING *;
-  `, items)
+  `, items);
 };
 exports.newListingPhoto = newListingPhoto;
 
+
 const setListingToSold = (listing) => {
   const query = [listing];
-
-  return db.query (`
+  return db.query(`
   UPDATE listings
     SET is_sold = true
     WHERE listings.id = $1;
-  `, query)
+  `, query);
 };
 exports.setListingToSold = setListingToSold;
+
 
 const viewListing = (id) => {
   const value = [id];
   return db.query(`
   SELECT * FROM listings
   JOIN photos ON listing_id = listings.id
-  WHERE listings.id = $1`, value)
-}
+  WHERE listings.id = $1`, value);
+};
 exports.viewListing = viewListing;
 
 
