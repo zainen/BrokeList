b1679be680a22cca11d625367140ce1962660b26
zainen <zainensuzuki@gmail.com>
2021-06-06 23:44:20 -0700

    con string

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -7,8 +7,7 @@ const chalk = require('chalk');
 const Client = require('pg-native');
 
 // PG connection setup
-const connectionString = process.env.DATABASE_URL
-  // || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=${process.env.DB_SSL}`;
 const client = new Client();
 
 // Loads the schema files from db/schema
