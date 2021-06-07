2bdfc5305e2c31c77492946575405fadeecaa602
zainen <zainensuzuki@gmail.com>
2021-06-06 20:33:37 -0700

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
+const PORT       = 62210
 const ENV        = process.env.ENV || 'development'; // not used
 const express    = require('express');
 const bodyParser = require('body-parser');
